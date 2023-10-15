91708a1484d6753cc9edcfcea2dc81a70b7880c9
KrazyKlaw <78502394+krzyklw@users.noreply.github.com>
2023-10-15 17:38:52 +0800

    app.js

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#000"
+        "value": "#14bc84"
       },
       "shape": {
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#000"
+          "color": "#14bc84"
         },
         "polygon": {
           "nb_sides": 5
@@ -50,7 +50,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#000",
+        "color": "#14bc84",
         "opacity": 0.4,
         "width": 1
       },
@@ -109,7 +109,7 @@ particlesJS('particles-js',
     "retina_detect": true,
     "config_demo": {
       "hide_card": false,
-      "background_color": "#b61924",
+      "background_color": "#14bc84",
       "background_image": "",
       "background_position": "50% 50%",
       "background_repeat": "no-repeat",
